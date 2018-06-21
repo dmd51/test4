89279f65e4bb28435774219ab004fa446cd97fc9
zaraa <zaraadmd@yahoo.com>
2018-06-22 07:27:42 +0800

    Initial sharing of project

---
+++ asd.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.lang.Math;
+
+public class asd {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n;
+        System.out.println("Works");
+    }
+}
